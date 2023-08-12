f5334dea0c927b8e3b62062fe03ab749c35cc63c
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2023-08-12 08:43:23 +0300

    Update CategoryCard.js

--- src/component/Category/CategoryCard.js
+++ src/component/Category/CategoryCard.js
@@ -3,7 +3,7 @@ import { Col } from 'react-bootstrap';
 
 function CategoryCard({background, img,title}) {
     return (
-        <Col xs= {12} sm={6} md={4} lg={2} className='my-4 d-flex justify-content-around'>
+        <Col xs= {6} sm={6} md={4} lg={2} className='my-4 d-flex justify-content-around'>
             <div className='allCard mb-3'>
                 <div className='category-card' style={{backgroundColor: `${background}`}}>
                 </div>
